a9fa93fb1f07dd3664cabb11b741f21fd79e4a03
magid mroueh <magidmroueh@gmail.com>
2020-02-27 09:28:20 +0200

    refactor: cleaning boilerplates (#1)
    * feat: update ReadMe
    * refactor: remove  not needed code

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const http = require('http');
+const debug = require('debug')('mercury-parser-express:server');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -33,7 +21,86 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
-module.exports = app;
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const Mercury = require('@postlight/mercury-parser');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Mercury Parser' });
+router.get('/', function(_, res) {
+  res.send({ title: 'Welcome to Mercury Parser API' });
 });
 
-router.get('/parse', async (req, res, next) => {
+router.get('/parse', async (req, res) => {
     const result = await Mercury.parse(req.query.url);
 
     res.send(result)
 });
 
-router.post('/parse-html', async (req, res, next) => {
+router.post('/parse-html', async (req, res) => {
   const result = await Mercury.parse(
     req.body.url,
     { contentType: 'html', html: req.body.html });
